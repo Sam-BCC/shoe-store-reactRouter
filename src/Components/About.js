f703c6e6f264e65572ba1a723d565fb2c6174036
Sam-BCC <65135395+Sam-BCC@users.noreply.github.com>
2020-07-15 12:48:20 +0300

    Update

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,7 +4,7 @@ function About() {
 
   return (
 	<div>
-		<h1>We bring you the best designs and comfortable shoes</h1>
+		<h1>We bring you the best designs and comfortable shoes from famous brands</h1>
 	</div>
   );
 }
